eddbf6754676a1ef59c3f32d18474438a7a52a4d
Matviishun <uljasichka@gmail.com>
2017-05-05 14:05:51 +0300

    js fixed, menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var page = location.hash;
+if (page == "#home"){
+    document.getElementById("home2").style = "background:black;"
+}
+else if (page == "#about"){
+    document.getElementById("about2").style = "background:black;"
+}
